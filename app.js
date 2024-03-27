2ed87bc2ad19266b7a14134dfe0f50f8fe361722
JoaoHenriqueBedin <jhbpdayko@minha.fag.edu.br>
2024-03-27 10:02:40 -0300

    Criando POST para atualizar planilha

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const xlsx = require('xlsx');
+const multer = require('multer');
 const port = 3000;
 
 const cors = require("cors");
 const app = express();
 app.use(cors());
+
+// Configuração do Multer para salvar o arquivo
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './'); // O diretório onde o arquivo será salvo
+    },
+    filename: (req, file, cb) => {
+        cb(null, 'Modulos.xlsx'); // Mantendo o mesmo nome de arquivo
+    }
+});
+const upload = multer({ storage: storage });
+
+// Rota para upload de arquivos
+app.post('/upload', upload.single('planilha'), (req, res) => {
+    res.json({ message: 'Arquivo recebido e salvo com sucesso!' });
+});
+
 
 app.get('/dados-clientes', (req, res) => {
     const workbook = xlsx.readFile('./Modulos.xlsx');
